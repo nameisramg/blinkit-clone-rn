6393b00640670b5de5f7f4b7ed063b9d4805b8b1
work <work@rams-Laptop.local>
2024-10-01 00:35:52 +0530

    Adding gesture control to delivery login

--- src/features/auth/CustomerLogin.tsx
+++ src/features/auth/CustomerLogin.tsx
@@ -1,14 +1,44 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React, { FC } from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { View, Text, StyleSheet, Animated } from 'react-native';
+import React, { FC, useState } from 'react';
+import { GestureHandlerRootView, PanGestureHandler, State } from 'react-native-gesture-handler';
 import CustomSafeAreaView from '@components/global/CustomSafeAreaView';
 import ProductSlider from '@components/login/ProductSlider';
+import { resetAndNavigate } from '@utils/NavigationUtils';
 const CustomerLogin: FC = () => {
+  const [gestureSequence, setGestureSequence] = useState<string[]>([]);
+
+  const handleGesture = ({ nativeEvent }: any) => {
+    if (nativeEvent.state === State.END) {
+      const { translationX, translationY } = nativeEvent;
+      let direction = '';
+      if (Math.abs(translationX) > Math.abs(translationY)) {
+        direction = translationX > 0 ? 'right' : 'left';
+      } else {
+        direction = translationY > 0 ? 'down' : 'up';
+      }
+      const newSequence = [...gestureSequence, direction].slice(-5);
+      setGestureSequence(newSequence);
+      console.log(newSequence);
+      if (newSequence.join(' ') === 'up up down left right') {
+        setGestureSequence([]);
+        resetAndNavigate('DeliveryLogin');
+      }
+    }
+  };
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
         <CustomSafeAreaView>
           <ProductSlider />
+          <PanGestureHandler onHandlerStateChange={handleGesture}>
+            <Animated.ScrollView
+              bounces={false}
+              keyboardDismissMode={'on-drag'}
+              keyboardShouldPersistTaps="handled"
+              contentContainerStyle={styles.subContainer}
+            ></Animated.ScrollView>
+          </PanGestureHandler>
         </CustomSafeAreaView>
       </View>
     </GestureHandlerRootView>
@@ -18,6 +48,12 @@ const CustomerLogin: FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  subContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignContent: 'center',
+    marginBottom: 20
   }
 });
 
